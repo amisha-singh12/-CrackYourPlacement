054ccb451c39d6cc4541015d83364a10e2ceaee8
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-04-02 07:07:48 +0530

    Add files via upload

---
+++ pattern/pattern3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"enter no";
+    cin>>n;
+    int row=1;
+   
+    while(row<=n){
+        int j=1;
+        int count=row;
+        while (j<=row)
+        {
+           cout<<count;
+            count++;
+           j++; /* code */
+        }
+       cout<<endl;
+       row++; 
+    }
+}
