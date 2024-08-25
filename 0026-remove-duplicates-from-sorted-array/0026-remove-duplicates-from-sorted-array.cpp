d3fbca2763efc0ab0a999fb9c8b6aa939294b4fb
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-08-25 11:21:25 +0530

    Time: 7 ms (70.5%), Space: 21.1 MB (80.56%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; // If the input array is empty, return 0
-        
-        int i = 0; // Initialize the first pointer
-        
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i]) { // When a new unique element is found
-                i++; // Increment the index for unique elements
-                nums[i] = nums[j]; // Update the position of the next unique element
-            }
+
+        if(nums.empty()) return 0;   // check whether array is empty or not 
+
+        int i = 0;
+        for( int j = 1; j < nums.size() ; j++ ) {  // put to iteratoer to keep track
+
+        // if they are equal then skip if not then increase i and swap its value with current j , by this all the duplicates will  be thrown backwards and only unique no with i index would be present.
+           if ( nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j] ;
+           }
         }
-        
-        return i + 1; // Return the count of unique elements
+        return i+1;
     }
 };
