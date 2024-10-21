d122e53b17d0c2e274e713e0203cc3eb32ed4b8e
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-10-21 06:52:40 +0530

    Time: 0 ms (100%), Space: 10.2 MB (76.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,35 @@
-#include <vector>
-
 class Solution {
 public:
-    void sortColors(std::vector<int>& nums) {
-        // dutch national flag algorithm 
-        // we simply are sorting based on comparison taking care of occurences.
-        int low = 0, mid = 0, high = nums.size() - 1;
-        
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                std::swap(nums[low], nums[mid]);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                std::swap(nums[mid], nums[high]);
-                high--;
-            }
-        }
+    void sortColors(vector<int>& nums) {
+       int count0 = 0 ;  
+       int count1 = 0 ;  
+       int count2 = 0 ; 
+        // first iteration we will simply count the no of occurences
+       for(int i = 0; i < nums.size() ; i++){
+        if(nums[i] == 0)
+          count0++;
+        else if(nums[i] == 1)
+          count1++;
+        else
+         count2++;
+       } 
+
+    //    in second count  we append the array basis on its count
+    // for loop ke andar karenge toh o(n*m) ho jayega isliye we will append them one by one
+
+    int index = 0;
+    for(int i = 0; i < count0 ; i++){
+      nums[index]= 0;
+      index++;
+    }  
+     for(int i = 0; i < count1 ; i++){
+      nums[index]= 1;
+      index++;
+    } 
+     for(int i = 0; i < count2 ; i++){
+      nums[index]= 2;
+      index++;
+    } 
+
     }
 };
