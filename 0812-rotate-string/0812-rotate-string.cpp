0289c298970683c76d2d3621ed8597ffe8242003
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-11-03 09:02:11 +0530

    Time: 0 ms (100%), Space: 7.7 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        // by checking it manually one by one method
-         if (s.size() != goal.size()) {
+        if (s.size() != goal.size()){
             return false;
         }
 
-         // Perform the rotation check
-        for (int i = 0; i < s.size(); ++i) {
-            // Move the first character to the end
-            char firstChar = s[0];
-            s.erase(s.begin()); // Remove the first character
-            s.push_back(firstChar); // Append it to the end
-            
-            // Check if the rotated string matches goal
-            if (s == goal) {
-                return true;
-            }
-        }
-         return false;
+    // rather than checking each term manually by appending and deleting we can use afollow up approach
+    //  in this we will simply Concatenate the string s with itself (s += s) allows us to cover all possible rotations in one step. 
+    // if goal is substing than its ture otherwise false
+
+    s += s ;
+
+     // Check if goal is a substring of the concatenated string
+        return s.find(goal) != string::npos;
     }
 };
