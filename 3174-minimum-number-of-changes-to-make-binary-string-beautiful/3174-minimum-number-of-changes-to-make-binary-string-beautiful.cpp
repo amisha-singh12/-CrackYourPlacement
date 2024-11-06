14de34f22c93d41fd33c93517446f5ad19d92c79
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-11-06 15:32:43 +0530

    Time: 0 ms (100%), Space: 12.1 MB (67.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     int minChanges(string s) {
-     int n = s.size();
-     int count = 0; 
-     char ch = s[0];
-     int changes = 0;
-
-     for(int i =0 ; i< n ; i++){
-        if(s[i] == ch){
-            count++;
-            continue;
+       int n = s.size();
+       int changes = 0 ;
+   
+     for(int i = 0; i < n ;i+=2){
+        if(s[i] != s[i+1]){
+            changes++;
         }
-        // current ch is not equal to ch
-        else{
-            if(count%2 == 0){  // previous substing is even len
-               count =1; // start a new substring
-            }
-            else{
-                // s[i] ko change akrna padega to make even len substring
-                changes += 1;
-                count =0 ;
-            }
-        }
-        ch = s[i];
-     }  
-     return changes; 
+     }
+     return changes;
     }
 };
