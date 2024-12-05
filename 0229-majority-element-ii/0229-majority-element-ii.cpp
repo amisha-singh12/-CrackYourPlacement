1aff4865b5f928ebd9250d415c23b93a55004c06
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2024-12-05 06:05:11 +0530

    Time: 1 ms (34.58%), Space: 20.1 MB (16.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,43 +1,16 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 0;
+    vector<int> majorityElement(vector<int>& nums)  {
+       unordered_map<int , int > mpp;
+       vector<int>a;
+       for(int num : nums){
+        mpp[num]++;
+       }
+       for(auto& [key , value]: mpp){
+            if(value > (nums.size()/3))
+              a.push_back(key);
+       }
 
-        // Step 1: Find candidates
-        for (int num : nums) {
-            if (count1 == 0 && num != candidate2) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0 && num != candidate1) {
-                candidate2 = num;
-                count2 = 1;
-            } else if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-
-        // Step 2: Verify candidates
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
-        }
-
-        // Step 3: Collect valid candidates
-        vector<int> result;
-        int threshold = nums.size() / 3;
-        if (count1 > threshold) result.push_back(candidate1);
-        if (count2 > threshold) result.push_back(candidate2);
-
-        return result;
+    return a; 
     }
-};
+};  
