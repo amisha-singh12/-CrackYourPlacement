0d0aed8af6234a02bb75f946d8a359b72f9905a9
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-01-05 14:27:29 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;  // Check if the input array is empty
 
-        if(nums.empty()) return 0;   // check whether array is empty or not 
+        vector<int> unique_nums;    // New vector to store unique elements
+        unique_nums.push_back(nums[0]); // Add the first element
 
-        int i = 0;
-        for( int j = 1; j < nums.size() ; j++ ) {  // put to iteratoer to keep track
-
-        // if they are equal then skip if not then increase i and swap its value with current j , by this all the duplicates will  be thrown backwards and only unique no with i index would be present.
-           if ( nums[i] != nums[j]) {
-            i++;
-            nums[i] = nums[j] ;
-           }
+        for (int i = 1; i < nums.size(); ++i) {
+            // Add only if the current number is different from the last added unique number
+            if (nums[i] != nums[i - 1]) {
+                unique_nums.push_back(nums[i]);
+            }
         }
-        return i+1;
+
+        // Copy the unique elements back into the original array
+        for (int i = 0; i < unique_nums.size(); ++i) {
+            nums[i] = unique_nums[i];
+        }
+
+        return unique_nums.size(); // Return the size of the unique array
     }
 };
