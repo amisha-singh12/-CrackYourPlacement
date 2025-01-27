c2f43504c81a7caf0ef3c101d756ee1424aa6f63
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-01-27 11:33:51 +0530

    Time: 0 ms (100%), Space: 9.9 MB (63.21%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void generateSubsets(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current); // Add the current subset to the result
+
+        for (int i = index; i < nums.size(); i++) {
+            // Skip duplicates
+            if (i > index && nums[i] == nums[i - 1]) continue;
+
+            // Include the current element in the subset
+            current.push_back(nums[i]);
+
+            // Recurse to the next element
+            generateSubsets(nums, i + 1, current, result);
+
+            // Backtrack (remove the last added element)
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+
+        // Sort the input to handle duplicates easily
+        sort(nums.begin(), nums.end());
+
+        // Start generating subsets
+        generateSubsets(nums, 0, current, result);
+
+        return result;
+    }
+};
