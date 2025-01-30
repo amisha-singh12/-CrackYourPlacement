53c98179b5892bf41592d98f9814dca18bec8694
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-01-30 07:55:39 +0530

    Time: 8 ms (20.93%), Space: 12.2 MB (22.08%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
    vector<vector<string>>result;
+     unordered_set<int> cols;
+     unordered_set<int> diag;
+     unordered_set<int> antidiag;
    int N;
 
-   bool isvalid(vector<string>& board, int row , int col){
-    // look upward
-    for(int i = row-1; i >= 0 ; i--){
-        if(board[i][col] == 'Q') {
-            return false;
-        }
-    }
 
-    // left diagoanl upwards
-    for(int i = row-1 , j = col-1; i >= 0 && j >= 0; i-- , j--){
-        if(board[i][j] == 'Q') {
-            return false;
-        }
-    }
-    // right diagonal upwards
-   for(int i = row-1 , j = col+1; i >= 0 && j < N; i-- , j++){
-        if(board[i][j] == 'Q') {
-            return false;
-        }
-    }
-    return true;
-   }
     void solve(vector<string>& board , int row){
       if(row  >= N){
          result.push_back(board);
          return;
         }
      for(int col = 0 ; col < N ; col++){
-        if(isvalid(board , row, col)){
-            board[row][col] = 'Q';
-            solve(board , row+1);
-            board[row][col] = '.';
+        int diagcons = row+col;
+        int antidiagcons = row-col;
+        if(cols.find(col) != cols.end() || diag.find(diagcons) != diag.end()  ||  antidiag.find(antidiagcons) != antidiag.end()){
+           continue;
         }
+        cols.insert(col);
+        diag.insert(diagcons);
+        antidiag.insert(antidiagcons);
+        board[row][col] = 'Q';
+
+        solve(board ,row+1);
+
+        cols.erase(col);
+        diag.erase(diagcons);
+        antidiag.erase(antidiagcons);
+        board[row][col] = '.';
      }
 }
     vector<vector<string>> solveNQueens(int n) {
