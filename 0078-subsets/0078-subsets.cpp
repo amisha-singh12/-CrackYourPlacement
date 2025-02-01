b91230d7abd018947a05b50a9db049cc60360363
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-01 06:32:58 +0530

    Time: 0 ms (100%), Space: 10.1 MB (47.1%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result = {{}};
+    vector<vector<int>>result;
 
-        for (int num : nums) {
-            int size = result.size();
-            for (int i = 0; i < size; i++) {
-                vector<int> subset = result[i];
-                subset.push_back(num);
-                result.push_back(subset);
-            }
+    void solve(int i , vector<int> & nums , vector<int> & temp){
+        if( i >= nums.size()){
+            result.push_back(temp);
+            return;
         }
+        // take ith element
+        temp.push_back(nums[i]); 
+        // trust with element
+        solve( i+1 , nums, temp);
+        // remove ith element
+        temp.pop_back();
+        // trust without element
+        solve( i+1 , nums, temp);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<int> temp;
+       solve( 0 ,  nums , temp);
+       return result;
 
-        return result;
     }
 };
