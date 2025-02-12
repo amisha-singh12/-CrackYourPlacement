2c535d377149f23e48095dcf0fd56c509614edec
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-12 15:20:52 +0530

    Time: 0 ms (100%), Space: 10.6 MB (99.06%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,15 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& res) {
-        if (root == nullptr) return;
-        helper(root->left, res);
-        res.push_back(root->val);
-        helper(root->right, res);
-    }
-
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        helper(root, res);
-        return res;
+        vector<int> result;
+        TreeNode* curr = root;
+
+        while(curr != NULL){
+            if(curr->left == NULL){
+                result.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* leftchild = curr-> left;
+                while(leftchild->right != NULL){
+                    leftchild =leftchild->right;
+                }
+                leftchild->right  = curr;
+                // curr left ko null mark kar sakte hai
+                TreeNode* temp = curr;
+                curr = curr->left;
+                temp ->left = NULL;
+
+            }
+        }
+        return result;
     }
 };
