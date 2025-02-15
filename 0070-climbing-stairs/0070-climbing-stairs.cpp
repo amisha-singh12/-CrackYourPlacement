3b033ef79ca3929429d864084867e95eafb5d4bd
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-16 01:10:22 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+// using recursion -memoization
+int t[46];
+    int solve(int n ){
+        if(n < 0) return 0;
+        if(t[n] != -1) return t[n];
+        if(n == 0) return 1; 
+
+        int onestep = solve(n-1);
+        int twostep = solve(n-2);
+
+        return t[n] = onestep + twostep;
+    }
+    int climbStairs(int n) {
+       memset( t , -1 , sizeof(t));
+       return solve(n); 
+    }
+};
