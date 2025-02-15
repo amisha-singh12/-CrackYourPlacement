46697cfa20d03c6ca5b9fb042daee61754f5890a
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-16 00:14:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (9.78%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-// constant space complexity
+// bottom up approach of dp
 // time complexity o(n)
     int fib(int n) {
-        if(n <= 1) return  n ;
-        int a =0 ,b =1 ;
-        int c ;
-        for(int i = 1 ; i <n ; i++ ){
-            c =a+b;
-            a = b , b =c;
+        if(n <= 1) return  n;
+        vector<int>dp(n+1 , -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i =2 ; i<=n ; i++ ){
+             dp[i] = dp[i-1] + dp[i-2];
         }
-        return c;
+      return dp[n];
     }
 };
