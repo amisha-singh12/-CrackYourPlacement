0979ea8b133c56a397e564aa6d759e9ecc5d8157
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-17 06:27:21 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    long long rob(vector<int>& nums) {
-      int n = nums.size() ;
-       if(n ==1 ) return  nums[0];
-      
-      vector<int> t(n+1 , 0);
-    //   t[i] money churai hai if have i houses
-      t[0] = 0;
-    
-    // case1 first house ko take kiya last ko skip
-    for(int i =1 ; i <= n-1 ;i++){
-        int skip = t[i-1];
-        int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
-        t[i] = max (steal ,skip);
-    } 
-    int result1 = t[n-1];
-    t.clear();
+int  t[101];
+   int solve(vector<int>& nums , int i ,int n  ){
+    if(i > n )return 0;
+    if(t[i] != -1) return t[i];
+    int steal = nums[i] + solve(nums , i+2, n);
+    int skip =  solve(nums , i+1, n);
 
-    // case2 first house ko skip kiya last ko take
-        t[0] = 0;
-        t[1] = 0; //skip ist house
-    for(int i = 2 ; i <= n ;i++){
-        int skip = t[i-1];
-        int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
-         t[i] = max (steal ,skip);
-    } 
-    int result2 = t[n];
-   
-    return max(result1 , result2);
+    return t[i] = max(steal ,skip);
+   }
+    int rob(vector<int>& nums) {
+      int n = nums.size(); 
+      if(n == 1) return nums[0];
+       if(n == 2) {
+        return max(nums[0] ,nums[1]);
+       }
+      memset(t  ,-1, sizeof(t));
+    //   case1 taking 0th index and skippinf the last house
+    int take_0thindex =  solve(nums , 0, n-2); 
+       
+        memset(t  ,-1, sizeof(t));
+    // case2 taking 1st index and including the last house
+    int take_1stindex = solve(nums , 1 , n-1);
+
+    return max(take_0thindex  ,  take_1stindex);
     }
 };
