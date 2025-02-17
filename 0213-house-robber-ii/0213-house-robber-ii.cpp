e9ce823b579b0c4d17287290e85118f2caa32905
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-17 06:34:33 +0530

    Time: 0 ms (100%), Space: 10 MB (97.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-int  t[101];
-   int solve(vector<int>& nums , int i ,int n  ){
-    if(i > n )return 0;
-    if(t[i] != -1) return t[i];
-    int steal = nums[i] + solve(nums , i+2, n);
-    int skip =  solve(nums , i+1, n);
 
-    return t[i] = max(steal ,skip);
+   int solve(vector<int>& nums , int l ,int r  ){
+    int prevofprev = 0;
+    int prev = 0; 
+
+    for(int i = l ; i <= r ; i++){
+        int steal = nums[i] + prevofprev;
+        int skip = prev;
+         
+       int temp = max(steal ,skip); 
+      prevofprev = prev;
+      prev =temp;
    }
+   return prev;
+}
     int rob(vector<int>& nums) {
       int n = nums.size(); 
       if(n == 1) return nums[0];
        if(n == 2) {
         return max(nums[0] ,nums[1]);
        }
-      memset(t  ,-1, sizeof(t));
     //   case1 taking 0th index and skippinf the last house
     int take_0thindex =  solve(nums , 0, n-2); 
        
-        memset(t  ,-1, sizeof(t));
     // case2 taking 1st index and including the last house
     int take_1stindex = solve(nums , 1 , n-1);
 
