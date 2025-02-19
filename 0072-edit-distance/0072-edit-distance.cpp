f5a8ca4e460ff5cb2f3b3ac994096a9df43f001e
Amisha Kumari Singh <144824700+amisha-singh12@users.noreply.github.com>
2025-02-19 14:41:33 +0530

    Time: 45 ms (17.8%), Space: 11.6 MB (85.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int m ,n ;
     int t[501][501];
-    int solve(string &word1, string &word2 , int i , int j){
-        if( i == m){
-           return n -j; //insert in word1
-        }
-        else if(j == n){
-          return m -i;  //insert in word2
-        }
+    int solve(string &word1, string &word2 , int m , int n){
+     if( m ==0 || n ==0 ){
+        return m+n ;
+     }
+        if(t[m][n] != -1) 
+         return t[m][n];
 
-        if(t[i][j] != -1) 
-         return t[i][j];
-
-        if(word1[i] == word2[j]){
-            return t[i][j] = solve(word1 ,word2 , i+1 , j+1);
+        if(word1[m-1] == word2[n-1]){
+            return t[m][n] = solve(word1 ,word2 , m-1 , n-1);
         }
         else{
-            int insertc  = 1 + solve(word1 ,word2 , i , j+1); 
-            int deletec  = 1 + solve(word1 ,word2 , i+1 , j);
-            int replacec = 1 + solve(word1 ,word2 , i+1 , j+1);   
+            int insertc  = 1 + solve(word1 ,word2 , m , n-1); 
+            int deletec  = 1 + solve(word1 ,word2 , m-1 , n);
+            int replacec = 1 + solve(word1 ,word2 , m-1 , n-1);   
 
-            return t[i][j] = min({insertc , deletec , replacec});
+            return t[m][n] = min({insertc , deletec , replacec});
         }
         return -1;
     }
@@ -29,6 +25,6 @@ public:
          m = word1.size();
          n = word2.size();
         memset(t ,-1, sizeof(t));
-        return solve(word1 , word2 ,0 ,0);
+        return solve(word1 , word2 ,m ,n);
     }
 };
